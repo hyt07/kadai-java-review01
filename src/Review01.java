ce9488f1b13a653831470323b594e50fc210c258
hyt07 <leven.tse.leve26550623@gmailcom>
2023-11-09 20:20:58 +0900

    taxPriceを追加

--- src/Review01.java
+++ src/Review01.java
@@ -5,8 +5,9 @@ public class Review01 {
         int price = 1500;
         double tax = 0.1;
         int total;
-        total = price + tax(price, tax);
-        System.out.println(price + "円の商品の税込み価格は" + total + "円(消費税は" + tax(price, tax) + "円)です。");
+        int taxPrise =tax(price, tax);
+        total = price + taxPrise;
+        System.out.println(price + "円の商品の税込み価格は" + total + "円(消費税は" + taxPrise + "円)です。");
 
     }
     public static int tax(int price, double tax) {
